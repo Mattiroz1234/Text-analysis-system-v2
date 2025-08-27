7c91ea1a1231d8a0435d0cd9955a44d10f68c8f1
Yossi Ben Haim <v0585204770@gmail.com>
2025-08-27 10:40:06 +0300

    create the basic pub and sub and name of punc in cleaning data

---
+++ cleaning_data/cleaning/cleaning_data.py
@@ -0,0 +1,30 @@
+class Cleaning_data:
+
+    def __init__(self):
+        pass
+
+    def removing_punctuation_marks(self):
+        pass
+
+    def removing_special_characters(self):
+        pass
+
+    def removing_unnecessary_whitespace(self):
+        """
+        ○ Removing unnecessary whitespace characters (tabs, long space sequences, all kinds of end-of-line characters).
+        :return:
+        """
+        pass
+
+    def removing_stop_words(self):
+        pass
+
+    def converting_text_to_lowercase(self):
+        pass
+
+    def lemtization(self):
+        """
+        ○ Lemtization (finding roots)
+        :return:
+        """
+        pass
