d5d051cd0ec076ced9e91634913417c9fd62369b
Yossi Ben Haim <v0585204770@gmail.com>
2025-08-28 17:02:47 +0300

    added the .bat

--- a.bat
+++ a.bat
@@ -14,3 +14,9 @@ docker run --name preprocessor-contaienier -d docker-image-preprocessor:v1
 
 
 docker build -t docker-image-enricher:v1 .
+
+docker run --name persister-contaienier -d docker-image-data-persister:v1
+
+docker build -t docker-image-data-persister:v1 .
+
+docker run --name persister-contaienier -d docker-image-data-persister:v1
